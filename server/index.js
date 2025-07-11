adfc821f73f3fab0bf699707ab5e122c2118d2c2
RitochitGhosh <ghosh.18.ritochit12@gmail.com>
2025-07-11 19:03:07 +0530

    Setting up backend: models, controllers, routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -12,6 +13,13 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use("/api/user", userRouter);
+
+
+app.get("/api/", (req, res) => {
+    res.send("API route is working!")
+})
 
 app.listen(port, () => {
     console.log(`Server is running on : http://localhost:${port}`);
